dad4e795ddaff8d6bdef66feeaf7effe53baf202
Anda Gavrilescu <andagavrilescu@wustl.edu>
2019-09-07 00:58:21 -0400

    js form

--- src/app/form/entry.form.js
+++ src/app/form/entry.form.js
@@ -4,7 +4,19 @@ document.getElementById("").addEventListener("submit", function(event) {
 
     var placeSearch = new XMLHttpRequest();
     var apiKey = AIzaSyDVLzXVT1vBPMgHF-x3MOXQlMJiVlryDj8;
+    var queryString = queryId.value; //should sanitize later
+    var minPrice = minId.value; //0 to 4
+    var maxPrice = maxId.value; //0 to 4
+    var radius = radiusId.value; //0 to 50,000 m
 
-    placeSearch.open('GET', 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=' + input + '&inputtype=textquery&fields=...&key=' + apiKey)
+    placeSearch.open('GET', 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=' + queryString + '&minprice=' + minPrice
+     + '&maxprice=' + maxPrice + '&opennow=true&radius=' + radius + '&key=' + apiKey);
+
+    placeSearch.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var results = JSON.parse(this.responseText);
+            
+        }
+    }
 
 })
