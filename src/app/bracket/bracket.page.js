8c7f598e02c47745855c72d9436da03839d2b4a8
Justin Hurley <justinhurley@huntsman-ve702-3105.apng.wlan.private.upenn.edu>
2019-09-07 10:34:39 -0400

    made working brackets for 16 and 8

--- src/app/bracket/bracket.page.js
+++ src/app/bracket/bracket.page.js
@@ -1,17 +1,50 @@
 var windowDim = {
-    width: window.innerWidth,
-    height: window.innerHeight,
-    center: width/2
-}
+    W: window.innerWidth,
+    H: window.innerHeight,
+    center: window.innerWidth/2
+};
+var canvasMargin = {
+    W: 100,
+    H: 100
+};
+var canvasMake = {
+    W: windowDim.W,
+    H: windowDim.H
+};
 lineWidth = 10;
-
+bracketHeight = 50;
 function makeBracket(bracket){
-    p5.rect(bracket.X-(bracket.W/2),bracket.Y,bracket.W,lineWidth);
+    var x0 = bracket.X-(bracket.W/2);
+    fill("#000000");
+    rect(x0,bracket.Y,bracket.W,lineWidth);
+    rect(x0, bracket.Y+lineWidth,lineWidth,bracketHeight);
+    rect(x0+bracket.W-lineWidth, bracket.Y+lineWidth, lineWidth, bracketHeight);
 }
-var maxLines = 8;
-testPos = {
-    X: 100,
-    Y: 100,
-    W: 10
+
+function make(brackets, rowH, marginW){
+    var width = (canvasMake.W - 2*marginW)/(brackets-1);
+    var x = marginW + width/2;
+    for(var i = 0; i < brackets; i++){
+        var newBracket = {
+            X: x,
+            Y: rowH,
+            W: width
+        }
+        makeBracket(newBracket);
+        x += width*2;
+    }
+    if(brackets == 1){
+        rect(x, rowH, lineWidth, bracketHeight);
+        return;
+    }
+    else{
+        make(brackets/2,rowH-bracketHeight,marginW+width/2-lineWidth/4);
+    }
 }
-makeBracket(testPos);
+
+function setup(){
+    createCanvas(windowDim.W, windowDim.H);
+}
+function draw(){
+   make(16, 400, 100);
+}
