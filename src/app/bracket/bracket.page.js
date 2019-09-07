e76022caf6175753ad86c6a01b2cce036295237d
Justin Hurley <justinhurley@huntsman-ve702-3105.apng.wlan.private.upenn.edu>
2019-09-07 18:22:14 -0400

    push>

--- src/app/bracket/bracket.page.js
+++ src/app/bracket/bracket.page.js
@@ -14,97 +14,145 @@ var canvasMake = {
 lineWidth = windowDim.W/150;
 bracketHeight = windowDim.H/8;
 var bracketQueue = [];
-function makeBracket(bracket){
-    var x0 = bracket.X;
-    fill("#000000");
-    rect(x0,bracket.Y,bracket.W,lineWidth);
-    rect(x0, bracket.Y+lineWidth,lineWidth,bracketHeight);
-    rect(x0+bracket.W, bracket.Y, lineWidth, bracketHeight+lineWidth);
-    bracketQueue.push(bracket);
-}
 
-function make(brackets, rowH, width){
-    var x = width/2;
-    if(brackets == 1){
-        rect(x, rowH, lineWidth, bracketHeight);
-        return;
-    }
-    else{
-        make(brackets/2,rowH-bracketHeight, width*2);
-    }
-    for(var i = 0; i < brackets/2; i++){
-        var newBracket = {
-            X: x,
-            Y: rowH,
-            W: width
-        }
-        makeBracket(newBracket);
-        x += width*2;
-    }
-}
+
 
 var scaleVal = 1;
 var y = windowDim.H/2;
 var w = windowDim.W/9;
 var tX = w/2;
 var tY = 0;
-function setup(){
-    createCanvas(windowDim.W, windowDim.H);
-    bracketNum = 0;
+let bracketSketch = function(p){
+    function makeBracket(bracket){
+        var x0 = bracket.X;
+        p.fill("#000000");
+        p.rect(x0,bracket.Y,bracket.W,lineWidth);
+        p.rect(x0, bracket.Y+lineWidth,lineWidth,bracketHeight);
+        p.rect(x0+bracket.W, bracket.Y, lineWidth, bracketHeight+lineWidth);
+        bracketQueue.push(bracket);
+    }
+    function make(brackets, rowH, width){
+        var x = width/2;
+        if(brackets == 1){
+            p.rect(x, rowH, lineWidth, bracketHeight);
+            return;
+        }
+        else{
+            make(brackets/2,rowH-bracketHeight, width*2);
+        }
+        for(var i = 0; i < brackets/2; i++){
+            var newBracket = {
+                X: x,
+                Y: rowH,
+                W: width
+            }
+            makeBracket(newBracket);
+            x += width*2;
+        }
+    }
+    p.setup = function(){
+        p.createCanvas(windowDim.W, windowDim.H);
+        bracketNum = 0;
+    }
+    var scaleFactor = 2.7;
+    p.draw = function(){
+        p.background("rgb(255,255,255)");
+        p.scale(scaleVal);
+        p.translate(tX, tY);
+        make(8, y, w);
+    }
+    var pressCount = 0;
+    var brackets = 8;
+    p.mousePressed = function(){
+        p.redraw();
+        bracketMover(pressCount);
+        pressCount++;
+    }
+    function bracketMover(pressCount){
+        if(pressCount == 0){
+            scaleVal = scaleVal*scaleFactor;
+            y = y/scaleVal;
+        }
+        else if(pressCount >= 1 && pressCount < brackets/2){
+            tX -=w*2;
+        }
+        else if(pressCount == brackets/2){
+            tX = w/2;
+            tY = 0;
+            scaleVal = 1;
+            y = windowDim.H/2;
+        }
+        else if(pressCount == brackets/2+1){
+            scaleVal = scaleVal*2;
+            tX = w/4;
+            y = y/scaleVal;
+        }
+        else if(pressCount == brackets/2+2){
+            tX -=w*4;
+        }
+        else if(pressCount == brackets/2+3){
+            tX = w/2;
+            tY = 0;
+            scaleVal = 1;
+            y = windowDim.H/2;
+        }
+        else if(pressCount == brackets/2+4){
+            tY += bracketHeight;
+    
+        }
+        else{
+            tX = w/2;
+            tY = 0;
+            scaleVal = 1;
+            y = windowDim.H/2;
+            pressCount = 0;
+        }
+    }
 }
-var scaleFactor = 2.7;
-function draw(){
-    background("rgb(255,255,255)");
-    scale(scaleVal);
-    translate(tX, tY);
-    make(8, y, w);
-}
-var pressCount = 0;
-var brackets = 8;
-function mousePressed(){
-    redraw();
-    background("#FFFFFF");
-    bracketMover(pressCount);
-    pressCount++;
-}
-function bracketMover(pressCount){
-    if(pressCount == 0){
-        scaleVal = scaleVal*scaleFactor;
-        y = y/scaleVal;
+let sketch1 = new p5(bracketSketch);
+let boxSketch = function(p){
+    var boxMargin = {
+        top: 50,
+        left: 50,
+        right: 50,
+        bottom: 50
     }
-    else if(pressCount >= 1 && pressCount < brackets/2){
-        tX -=w*2;
+    var boxPadding = {
+        top: 50,
+        left: 50,
+        right: 50,
+        bottom: 50
     }
-    else if(pressCount == brackets/2){
-        tX = w/2;
-        tY = 0;
-        scaleVal = 1;
-        y = windowDim.H/2;
+    var boxDim = {
+        H: windowDim.H*0.8 - boxMargin.top - boxMargin.bottom,
+        W: windowDim.W/2 - boxMargin.left - boxMargin.right
     }
-    else if(pressCount == brackets/2+1){
-        scaleVal = scaleVal*2;
-        tX = w/4;
-        y = y/scaleVal;
-        
+    function leftBox(){
+        p.rect(boxMargin.left, boxMargin.top, boxDim.H, boxDim.W);
+        p.textSize(30);
+        p.text("Name Goes Here", boxMargin.left + boxPadding.left, windowDim.H/2)
+        p.text("4.5")
     }
-    else if(pressCount == brackets/2+2){
-        tX -=w*4;
+    function rightBox(){
+        p.rect(windowDim.W - boxDim.W + boxMargin.left + boxMargin.right, boxMargin.top, boxDim.H, boxDim.W);
     }
-    else if(pressCount == brackets/2+3){
-        tX = w/2;
-        tY = 0;
-        scaleVal = 1;
-        y = windowDim.H/2;
+    function vsDraw(){
+        var tSize = 60;
+        p.textSize(tSize);
+        p.text("VS", windowDim.W/2-tSize/2, windowDim.H/2);
+        p.fill("#000000");
     }
-    else if(pressCount == brackets/2+4){
-        tY += bracketHeight;
-
+    p.setup = function(){
+        p.createCanvas(windowDim.W, windowDim.H);
+    }
+    p.draw = function(){
+        leftBox();
+        rightBox();
+        vsDraw();
+        clear();
     }
 }
-
-
-
-
+let sketch2 = new p5(boxSketch);
     // var storedResults = JSON.parse(localStorage.getItem("results"));
     // console.log(storedResults);
 
